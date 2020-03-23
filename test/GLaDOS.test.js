d9bdacfb65a91f7ea658083a7f8a8ebfc7a8193a
c-noblet <c.noblet.dev@gmail.com>
2020-03-23 21:05:57 +0000

    renamed the GLaDOS test file into glados

--- test/GLaDOS.test.js
+++
@@ -1,3 +0,0 @@
-process.env.NTBA_FIX_319 = 1;
-const GLaDOS = require('./glados');
-const env = require('./env.json');
---
+++ tests/glados.test.js
@@ -0,0 +1,3 @@
+process.env.NTBA_FIX_319 = 1;
+const GLaDOS = require('../glados');
+const env = require('../env.json');
