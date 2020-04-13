c1153c67c456e56d1072a389f590b6df2dc7f6f2
c-noblet <c.noblet.dev@gmail.com>
2020-04-13 14:04:06 +0000

    command handler

---
+++ src/commands/commandContext.ts
@@ -0,0 +1,24 @@
+// Récupère les informations d'une commande
+export class CommandContext {
+  readonly command: string;
+  readonly args: string[];
+  readonly context: any;
+  readonly prefix: string;
+  readonly app: string;
+
+  constructor(context: any, prefix: string) {
+    let args = [];
+    if(context.content){
+      this.app = 'discord';
+      args = context.content.slice(prefix.length).trim().split(/ +/g);
+    }else{
+      this.app = 'telegram';
+      args = context.message.text.slice(prefix.length).trim().split(/ +/g);
+    }
+    
+    this.command = args.shift().toLowerCase();
+    this.args = args;
+    this.context = context;
+    this.prefix = prefix;
+  }
+}
