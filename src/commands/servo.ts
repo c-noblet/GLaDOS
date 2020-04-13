3bced86855df7ca56b1e59b6519c8fd655049c63
c-noblet <c.noblet.dev@gmail.com>
2020-04-13 16:23:27 +0000

    returned sentence updated

--- src/commands/servo.ts
+++ src/commands/servo.ts
@@ -10,7 +10,7 @@ export class Servo implements Command {
       const move = spawn('python',["./build/lib/servo.py", commandContext.args[0].toString()]);
       move.stdout.on('data', (data) => {
         if(data.toString().search('moved') >=0){
-          resolve(`Le servomoteur à tourné de ${commandContext.args[0]} degrés`);
+          resolve(`Le servomoteur a tourné de ${commandContext.args[0]} degrés`);
         }
       });
     });
