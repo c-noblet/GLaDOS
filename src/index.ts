723476339d05bfdcbc3c7b48c1835a00a4023fff
c-noblet <c.noblet.dev@gmail.com>
2020-04-13 14:05:18 +0000

    all service launcher added

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import DiscordServer from './discord';
+import TelegramServer from './telegram';
+import ExpressServer from './express';
+import { quarterJob } from './crons';
+import env from './env.json';
+
+TelegramServer.launch();
+DiscordServer.login(env.discord.token);
+quarterJob.start();
+ExpressServer.listen(env.express.port, () => {
+  console.log(`Express à l'écoute sur le port ${env.express.port}`, );
+});
+
+
