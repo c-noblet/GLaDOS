db95aefc92c2382ea73bd4f167a0e1b6cc0ad453
c-noblet <c.noblet.dev@gmail.com>
2020-05-25 15:26:20 +0000

    create the mqtt event handler

--- src/events/index.ts
+++ src/events/index.ts
@@ -1,3 +1,9 @@
 import events from 'events';
+import { telegram } from '../telegram';
+import env from '../env.json';
 
 export const eventEmitter = new events.EventEmitter();
+
+eventEmitter.on('mqtt', (data) => {
+  telegram.sendMessage(env.telegram.pm, data.click);
+})
