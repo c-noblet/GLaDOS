4af6b5c9a7e704830c5df169a80a22a7f3b49bce
c-noblet <c.noblet.dev@gmail.com>
2020-05-25 14:20:10 +0000

    create the getMqttLog function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,6 @@
 import { spawn } from 'child_process';
 import env from '../env.json';
+import { eventEmitter } from '../events'
 
 export const isHome = (names: string[]=[]): Promise<string[]> => {
   return new Promise((resolve) => {
@@ -32,5 +33,16 @@ export const isHome = (names: string[]=[]): Promise<string[]> => {
         resolve(results);
       }
     });
+  });
+}
+
+export const getMqttLog = async () => {
+  const mqttLog = spawn('journalctl',["-u", "zigbee2mqtt.service", "-f"]);
+  mqttLog.stdout.on('data', (data: any): void => {
+    const log = data.toString().trim().split("'");
+    const name = log[1].replace('zigbee2mqtt/','');
+    const deviceData = JSON.parse(log[3]);
+    deviceData.name = name;
+    eventEmitter.emit('mqtt', deviceData);
   });
 }
