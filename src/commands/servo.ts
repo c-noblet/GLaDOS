b7ba5db457edfde17f6942f48f0022eb7bd3d487
c-noblet <c.noblet.dev@gmail.com>
2020-06-13 17:04:58 +0000

    add multi servo motor handling

--- src/commands/servo.ts
+++ src/commands/servo.ts
@@ -5,10 +5,10 @@ export default {
   name: 'servo',
   description: 'is a test',
   run(context: CommandContext): void {
-    const move = spawn('python',["./build/lib/servo.py", context.args[0].toString()]);
+    const move = spawn('python',["./build/lib/servo.py", context.args[0].toString(), context.args[1].toString()]);
     move.stdout.on('data', (data) => {
       if(data.toString().search('moved') >=0){
-        context.appContext.reply(`Le servomoteur a tourné de ${context.args[0]} degrés`);
+        context.appContext.reply(`Le servomoteur ${context.args[0]} a tourné de ${context.args[1]} degrés`);
       }
     });
   }
--- src/lib/servo.py
+++ src/lib/servo.py
@@ -4,22 +4,29 @@ import time
 import sys
 import math
 import Adafruit_PCA9685
-
 import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(7,GPIO.OUT, initial=GPIO.HIGH)
 
-pwm = Adafruit_PCA9685.PCA9685(address=0x41)
-angle = int(sys.argv[1])
+# Set up
+channel = int(sys.argv[1]) # Target the servo motor
+angle = int(sys.argv[2])
+
 servo_min = 100  # Minimale Pulslaenge
 servo_mid = 300
 servo_max = 500  # Maximale Pulslaenge
-channel = 0
+
+# Connection to Talking pi board
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(7,GPIO.OUT, initial=GPIO.HIGH)
+pwm = Adafruit_PCA9685.PCA9685(address=0x41)
+
+# Convert degrees into pulse width
 ratio = (servo_max-servo_mid)/90
 if angle >= 0:
     pulse = int(math.ceil(ratio * angle + servo_mid))
 else:
     pulse = int(abs(math.ceil(ratio * abs(angle) - servo_mid)))
+
+# Move the Servo motor
 pwm.set_pwm_freq(50)
 pwm.set_pwm(channel, 0, pulse)
 print('moved')
