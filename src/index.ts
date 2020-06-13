0bc582159cecfdbae6bf69f3da33a32c3ea602d7
c-noblet <c.noblet.dev@gmail.com>
2020-06-13 17:01:20 +0000

    removed getMqttLog

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,8 @@ import DiscordServer from './discord';
 import { telegraf as TelegramServer } from './telegram';
 import ExpressServer from './express';
 import { quarterJob } from './crons';
-import { getMqttLog } from './utils';
 import env from './env.json';
 
-getMqttLog();
 TelegramServer.launch();
 DiscordServer.login(env.discord.token);
 quarterJob.start();
